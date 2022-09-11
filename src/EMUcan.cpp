341d40758270079d2c15fe270af68af427b978b4
Stephan Martin <designer2k2@gmail.com>
2022-09-11 18:36:22 +0200

    typo bugfix

--- src/EMUcan.cpp
+++ src/EMUcan.cpp
@@ -154,19 +154,19 @@ bool EMUcan::decodeEmuFrame(struct can_frame *msg) {
     emu_data.flexFuelEthanolContent = msg->data[7];
   }
   //Base +5:
-  if (msg->id == _EMUbase + 5) {
+  if (msg->can_id == _EMUbase + 5) {
     //0 DBW Pos 0.5%
-    emu_data.DBWpos = msg->buf[0] * 0.5;
+    emu_data.DBWpos = msg->data[0] * 0.5;
     //1 DBW Target 0.5%
-    emu_data.DBWtarget = msg->buf[1] * 0.5;
+    emu_data.DBWtarget = msg->data[1] * 0.5;
     //2-3 TC DRPM RAW 16bit  1/bit
-    emu_data.TCdrpmRaw = ((msg->buf[3] << 8) + msg->buf[2]);
+    emu_data.TCdrpmRaw = ((msg->data[3] << 8) + msg->data[2]);
     //4-5 TC DRPM 16bit  1/bit
-    emu_data.TCdrpm = ((msg->buf[5] << 8) + msg->buf[4]);
+    emu_data.TCdrpm = ((msg->data[5] << 8) + msg->data[4]);
     //6 TC Torque reduction %
-    emu_data.TCtorqueReduction = msg->buf[6];
+    emu_data.TCtorqueReduction = msg->data[6];
     //7 Pit Limit Torque reduction %
-    emu_data.PitLimitTorqueReduction = msg->buf[7];
+    emu_data.PitLimitTorqueReduction = msg->data[7];
   }
   //Base +6:
   if (msg->can_id == _EMUbase + 6) {
@@ -185,7 +185,7 @@ bool EMUcan::decodeEmuFrame(struct can_frame *msg) {
     //2 PWM#1 DC 1%/bit
     emu_data.pwm1 = msg->data[2];
     //3 DSG mode 2=P 3=R 4=N 5=D 6=S 7=M 15=fault
-    emu_data.DSGmode = msg->buf[3];
+    emu_data.DSGmode = msg->data[3];
     if (msg->can_dlc == 8) {
       //4 Lambda target 8bit 0.01%/bit
       emu_data.lambdaTarget = msg->data[4] * 0.01;
