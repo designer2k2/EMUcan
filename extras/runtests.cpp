dd7a78dfa35c8d1c79a8eae0d4e1df01dd7215a5
Stephan Martin <designer2k2@gmail.com>
2022-12-28 20:01:39 +0100

    Update runtests.cpp

--- extras/runtests.cpp
+++ extras/runtests.cpp
@@ -30,15 +30,30 @@ void delay_test() {
 }
 
 void run_tests() {
+  // Init the library:
   EMUcan emucan(0x600);
 
-  cout << "RPM: " << emucan.emu_data.RPM;
-
+  // Check if the init was ok, status has to be fresh:
   if (emucan.EMUcan_Status() == EMUcan_FRESH) {
-    cout << "EMUcan init ok";
+    cout << "EMUcan init ok" << endl;
   } else {
     throw std::runtime_error("EMUcan init not ok");
   }
+
+  // Generate a can frame and hand it over:
+  uint8_t data[8] = { 0x00, 0x0f, 0x13, 0x02, 0x00, 0x00, 0x08, 0x00 };
+  emucan.checkEMUcan(0x604, 8, canMsg.buf);
+
+
+  // Now the status has to be that something was received:
+  if (emucan.EMUcan_Status() == EMUcan_RECEIVED_WITHIN_LAST_SECOND) {
+    cout << "EMUcan init ok" << endl;
+  } else {
+    throw std::runtime_error("EMUcan init not ok");
+  }
+
+  // Based on the frame from above:
+  cout << "emuTemp: " << emucan.emu_data.emuTemp << endl;
 }
 
 int main(int argc, char **argv) {
